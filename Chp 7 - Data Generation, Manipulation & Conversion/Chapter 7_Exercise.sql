1aa7854866fe323b9867480350f96dcc9863b642
Ishita Shah <55583770+ishitashah23@users.noreply.github.com>
2020-09-10 23:04:54 +0530

    Data conversion exercise solutions

---
+++ Chp 7 - Data Generation, Manipulation & Conversion/Chapter 7_Exercise.sql
@@ -0,0 +1,11 @@
+-- Exercise 1 
+
+SELECT SUBSTRING('Please find the substring in this string', 17, 9);
+
+-- Exercise 2
+
+SELECT ABS(-25.76823) as ABSOLUTE, SIGN(-25.76823) as SIGN, ROUND(-25.76823,2)as ROUNDED;
+
+-- Exercise 3
+
+SELECT EXTRACT(MONTH FROM current_date()) AS CURRENT_MONTH;
