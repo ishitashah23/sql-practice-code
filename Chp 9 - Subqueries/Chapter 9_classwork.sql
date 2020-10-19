0795d745f3a400c8e596cc4b8594481731f9d11d
Ishita Shah <55583770+ishitashah23@users.noreply.github.com>
2020-10-19 17:06:35 +0530

    Added example for CTE

--- Chp 9 - Subqueries/Chapter 9_classwork.sql
+++ Chp 9 - Subqueries/Chapter 9_classwork.sql
@@ -38,3 +38,34 @@ WHERE
 (SELECT sum(p.amount) FROM payment p 
 WHERE p.customer_id = c.customer_id)
 BETWEEN 180 AND 240;
+
+-- CTE example
+
+WITH actors_s AS
+(SELECT actor_id, first_name, last_name 
+FROM actor 
+WHERE last_name LIKE 'S%'),
+
+actors_s_pg AS
+(SELECT s.actor_id, s.first_name, s.last_name, f.film_id, f.title
+FROM actors_s AS s 
+	INNER JOIN film_actor AS fa
+    ON s.actor_id = fa.actor_id
+    INNER JOIN film AS f
+    ON f.film_id = fa.film_id
+WHERE f.rating = 'PG'),
+
+actors_s_pg_revenue AS 
+(SELECT spg.first_name, spg.last_name, p.amount 
+FROM actors_s_pg AS spg
+	INNER JOIN inventory AS i
+    ON i.film_id = spg.film_id
+    INNER JOIN rental AS r
+    ON i.inventory_id = r.inventory_id
+    INNER JOIN payment AS p 
+    ON r.rental_id = p.rental_id)
+    
+SELECT spg_rev.first_name, spg_rev.last_name, sum(spg_rev.amount) AS tot_revenue
+FROM actors_s_pg_revenue AS spg_rev
+GROUP BY spg_rev.first_name, spg_rev.last_name
+ORDER BY 3 DESC;
